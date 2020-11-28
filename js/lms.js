ca89f17611ad0c7b18bb2779e2190fdb4fd787b7
00010059 <74616279+00010059@users.noreply.github.com>
2020-11-28 16:35:37 +0500

    updated ask page

--- js/lms.js
+++ js/lms.js
@@ -57,3 +57,17 @@ span4.addEventListener("click", function() {
  span6.addEventListener("click", function() {
     modal6.style.display = "none";
 }) 
+
+//grabbing button and navbar
+var btn = document.getElementById("logo");
+var screen = document.getElementById("screen");
+var nav = document.getElementById("nav");
+
+//adding click event
+btn.addEventListener("click", function() {
+    nav.classList.toggle("show");
+    if(screen.style.display == "block")
+    screen.style.display = "none";
+    else 
+    screen.style.display = "block"
+})
