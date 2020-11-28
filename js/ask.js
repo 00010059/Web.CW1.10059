e358c764197366c63ec3f42480179e8be9089caa
00010059 <74616279+00010059@users.noreply.github.com>
2020-11-28 16:36:04 +0500

    Update ask.js

--- js/ask.js
+++ js/ask.js
@@ -31,3 +31,17 @@ btn5.addEventListener("click", function() {
 btn6.addEventListener("click", function() {
     content6.classList.toggle("block");
 })
+
+//grabbing button and navbar
+var btn = document.getElementById("logo");
+var screen = document.getElementById("screen");
+var nav = document.getElementById("nav");
+
+//adding click event
+btn.addEventListener("click", function() {
+    nav.classList.toggle("show");
+    if(screen.style.display == "block")
+    screen.style.display = "none";
+    else 
+    screen.style.display = "block"
+})
